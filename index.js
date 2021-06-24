50a5a76ff05a326bcc9566b50f85a68ebd0c7c7a
unknown <h0p0phh0@gmail.com>
2021-06-24 21:22:37 +0700

    create branch dev

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-document.querySelector('#demo').innerHTML = 'master';
+function functionDev () { // Declare a function
+    document.getElementById("demo").innerHTML = "devBranch";
+  }
+   
+  functionDev ();
