267d44dd05c667436b7423c8c8be9a3b89a12153
unknown <h0p0phh0@gmail.com>
2021-06-24 21:37:13 +0700

    merge master and deploy

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-function functionDeploy () { // Declare a function
-    document.getElementById("demo").innerHTML = "testBranch";
-  }
-   
-  functionDeploy ();
-function functionCheckTask () { // Declare a function
-    document.getElementById("demo").innerHTML = "testTask";
-  }
-   
-  functionCheckTask ();
-  function testCompleted () { // Declare a function
-    document.getElementById("demo").innerHTML = "testComplete";
-  }
-   
-  testCompleted ();
+function functionDeploy() {
+  // Declare a function
+  document.getElementById("demo").innerHTML = "testBranch";
+}
+
+functionDeploy();
+function functionCheckTask() {
+  // Declare a function
+  document.getElementById("demo").innerHTML = "testTask";
+}
+
+functionCheckTask();
+function testCompleted() {
+  // Declare a function
+  document.getElementById("demo").innerHTML = "testComplete";
+}
+
+testCompleted();
+function doneTask() {
+  // Declare a function
+  document.getElementById("demo").innerHTML = "doneTask";
+}
+
+doneTask();
